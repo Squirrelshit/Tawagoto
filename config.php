948139db00affb2e1965c22d2ea8c68430eb9039
harukahimari <kob101@outlook.com>
2015-08-03 04:32:04 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/******************************************************
+------------------Required Configuration---------------
+Please edit the following variables so Tawagoto can
+work correctly.
+******************************************************/
+
+mysql_connect('host', 'user', 'password');
+mysql_select_db('database name');
+
+?>
